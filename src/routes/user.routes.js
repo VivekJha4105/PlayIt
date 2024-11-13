ffa7acd58caf346f05bdc7791e0af6b4b2ad53f8
Vivek Jha <vivek.ajay.p50@gmail.com>
2024-11-13 20:07:35 +0530

    added route for previous commit controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,14 +9,17 @@ import {
     updateAvatar,
     updateCoverImage,
     getCurrentUser,
+    getChannelByUsername,
+    getWatchHistoryOfUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+//* Registers a new user:
 router.route("/register").post(
-    //* Multer middleware to handle files upload
+    // Multer middleware to handle files upload
     upload.fields([
         {
             name: "avatar",
@@ -30,28 +33,35 @@ router.route("/register").post(
     registerUser
 );
 
-//! Authentication
+//! ==>> Authentication
 router.route("/login").post(loginUser);
 
-//! Token Refresh route for when accessToken has expired thus no auth middleware is needed.
+//! ==>> Token Refresh route for when accessToken has expired thus no auth middleware is needed.
 router.route("/token-refresh").post(refreshAccessToken);
 
-//! Below Routes need Authorization
-
+//! ==>> Below Routes need Authorization:
 router.route("/current-user").post(verifyJWT, getCurrentUser);
-
 router.route("/password-reset").post(verifyJWT, changePassword);
 
-router.route("/update-profile").post(verifyJWT, updateAccount);
+//* Updates either fullName or email, if not both
+router.route("/update-profile").patch(verifyJWT, updateAccount);
 
+//* Individual routes to update avatar and cover image separately.
 router
     .route("/update-avatar")
-    .post(verifyJWT, upload.single("avatar"), updateAvatar);
-
+    .patch(verifyJWT, upload.single("avatar"), updateAvatar);
 router
     .route("/update-cover-image")
-    .post(verifyJWT, upload.single("coverImage"), updateCoverImage);
+    .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
+//* getting info of any one, among many available channels of different users, by username.
+//* returns a resonse with subscribers count of that channel as well as subscribed channel count by the loggedin user.
+router.route("/channel/:username").get(verifyJWT, getChannelByUsername);
+
+//* Logged in users watch history.
+router.route("/watch-history").get(verifyJWT, getWatchHistoryOfUser);
+
+//* getting info of all the channels subscribed by the logged in user.
 router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
